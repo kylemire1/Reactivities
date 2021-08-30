cf55d7a5a3f1400ac2fd634b57653ffb17ad9eee
Kyle Lemire <hello@klemire.com>
2021-08-30 16:29:19 -0500

    chore: add comments for future reference

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -15,14 +16,32 @@ namespace Application.Activities
     public class Handler : IRequestHandler<Query, List<Activity>>
     {
       public DataContext _context;
-      public Handler(DataContext context)
+      private readonly ILogger _logger;
+      // Add logger if needed
+      // public Handler(DataContext context, ILogger<List> logger)
+      public Handler(DataContext context, ILogger<List> logger)
       {
+        _logger = logger;
         _context = context;
       }
 
       public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
       {
-        return await _context.Activities.ToListAsync();
+        // How to use cancellation token
+        // try
+        // {
+        //   for (var i = 0; i < 10; i++)
+        //   {
+        //     cancellationToken.ThrowIfCancellationRequested();
+        //     await Task.Delay(1000, cancellationToken);
+        //     _logger.LogInformation($"Task {i} has completed");
+        //   }
+        // }
+        // catch (System.Exception ex) when (ex is TaskCanceledException)
+        // {
+        //   _logger.LogInformation("Task was cancelled");
+        // }
+        return await _context.Activities.ToListAsync(cancellationToken);
       }
     }
   }
